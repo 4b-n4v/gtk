190caac6da0138ea9d312277021d58086f57ec3b
4b-n4v <ab.navilon@gmail.com>
2024-08-04 20:40:05 -0500

    radiobuttons

--- main.cc
+++ main.cc
@@ -1,10 +1,10 @@
-#include "examplewindow.h"
+#include "radiobuttons.h"
 #include <gtkmm/application.h>
 
 int main(int argc, char *argv[])
 {
-	auto app = Gtk::Application::create("org.gtkmm.example");
+        auto app = Gtk::Application::create("org.gtkmm.example");
 
-	// Shows the window and returns when it is closed.
-	return app->make_window_and_run<ExampleWindow>(argc, argv);
+        // Shows the window and returns when it is closed.
+        return app->make_window_and_run<RadioButtons>(argc, argv);
 }
